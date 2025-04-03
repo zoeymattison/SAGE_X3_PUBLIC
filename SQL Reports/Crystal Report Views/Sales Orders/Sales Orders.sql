dfce139d1d3c3a6fa82ec91202997f0f2716d503
zoeymattison <122073280+zoeymattison@users.noreply.github.com>
2025-04-03 13:27:09 -0700

    SQL updates

--- SQL Reports/Crystal Report Views/Sales Orders/Sales Orders.sql
+++ SQL Reports/Crystal Report Views/Sales Orders/Sales Orders.sql
@@ -63,67 +63,6 @@ CreditCardInfo AS (
         LANMES_0,
         ACCNCKNAM_0,
         STAFLG_0
-),
-RankedCreditCardInfo AS (
-    SELECT
-        SOHNUM_0,
-        ACCL4D_0,
-        PAYAMT_0,
-        AUTAMT_0,
-        TAXAMT_0,
-        LANMES_0,
-        ACCNCKNAM_0,
-        ROW_NUMBER() OVER (PARTITION BY SOHNUM_0, ACCL4D_0 ORDER BY STAFLG_0 DESC) AS rn
-    FROM CreditCardInfo
-),
-PrepayemntInfo AS (
-    SELECT
-        DUD.NUM_0,
-        DUD.PAMTYP_0,
-        SUM(DUD.AMTCUR_0) AS AMTCUR_0,
-        SUM(DUD.PAYCUR_0) AS PAYCUR_0,
-        SEU.ACCL4D_0,
-        SUM(SEU.PAYAMT_0) AS PAYAMT_0,
-        SUM(SEU.AUTAMT_0) AS AUTAMT_0,
-        SUM(SEU.TAXAMT_0) AS TAXAMT_0,
-        APL.LANMES_0,
-        SEU.ACCNCKNAM_0,
-        SEU.STAFLG_0
-    FROM
-        LIVE.GACCDUDATE DUD
-    LEFT JOIN
-        LIVE.PAYMENTD PYD ON DUD.NUM_0 = PYD.VCRNUM_0
-    LEFT JOIN
-        LIVE.SEAUTH SEU ON PYD.NUM_0 = SEU.VCRNUM_0
-    LEFT JOIN
-        LIVE.APLSTD APL ON SEU.STAFLG_0 = APL.LANNUM_0
-        AND APL.LANCHP_0 = '2095'
-        AND APL.LAN_0 = 'ENG'
-    WHERE
-        DUD.PAMTYP_0 = 2
-        AND (SEU.STAFLG_0 IN (3, 6) OR SEU.STAFLG_0 IS NULL)
-    GROUP BY
-        DUD.NUM_0,
-        DUD.PAMTYP_0,
-        SEU.ACCL4D_0,
-        APL.LANMES_0,
-        SEU.ACCNCKNAM_0,
-        SEU.STAFLG_0
-),
-RankedPrepaymentInfo AS (
-    SELECT
-        NUM_0,
-        PAMTYP_0,
-        AMTCUR_0,
-        PAYCUR_0,
-        ACCL4D_0,
-        PAYAMT_0,
-        AUTAMT_0,
-        TAXAMT_0,
-        LANMES_0,
-        ACCNCKNAM_0,
-        ROW_NUMBER() OVER (PARTITION BY NUM_0, ACCL4D_0 ORDER BY STAFLG_0 DESC) AS rn
-    FROM PrepayemntInfo
 )
 SELECT
     /* SALES ORDER */
